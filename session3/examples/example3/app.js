7b81ced9741d26cf8ed6780545102a6c50fe9fba
AntonNarkevich <17140th@gmail.com>
2015-04-08 01:45:34 +0300

    Updates examples

--- session3/examples/example3/app.js
+++ session3/examples/example3/app.js
@@ -1,4 +1,3 @@
-var fs = require("fs");
 var path = require("path");
 var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
@@ -22,55 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('swig', swig.renderFile);
 app.set('view engine', 'swig');
 
-app.get("/info", function (req, res, next) {
-	fs.readFile("info.txt", function (err, text) {
-		if (err) {
-			return next(err);
-		}
-
-		res.send(text);
-	});
-});
-
-app.get("/giveMeSession", function (req, res, next) {
-	req.session.someValue = 3;
-	res.cookie("cookieValue", 42);
-
-	res.render("cookie");
-});
-
-app.post("/:alias/:id",
-	function (req, res, next) {
-		req.startTime = new Date();
-		next();
-	},
-	function (req, res, next) {
-		var params = {
-			query: req.query,
-			body: req.body, //No value here
-			route: req.params,
-			cookie: req.cookies, //No value here
-			auhtHeader: req.get("Authorization")
-		};
-
-		console.log("Processed request %j", params);
-
-		res.json(params);
-		next();
-	},
-	function (req, res, next) {
-		req.endTime = new Date();
-		var processingTime = req.endTime - req.startTime;
-		console.log("Processing time %d", processingTime);
-	},
-	function (req, res, next) {
-		console.log("This function will never be called.");
-	}
-);
-
-app.get("/error", function (req, res, next) {
-	next(new Error("I want to raise error here"));
-});
+app.use('/', require("./routes/testRoutes"));
 
 app.use(function (err, res, res, next) {
 	res.status(500).render("error", {err: err});
